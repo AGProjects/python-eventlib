b01426bd458f4ce76904560f64cc12037f9d6bbf
Dan Pascu <dan@ag-projects.com>
2010-11-09 17:05:43 +0000

    Adjusted project information to indicate that is a derivative project

--- setup.py
+++ setup.py
@@ -7,28 +7,28 @@ from eventlet import __version__
 def find_packages(toplevel):
     return [directory.replace('/', '.') for directory, subdirs, files in os.walk(toplevel) if '__init__.py' in files]
 
-setup(
-    name='python-eventlet-0.8',
-    version=__version__,
-    description='Coroutine-based networking library',
-    author='Linden Lab',
-    author_email='eventletdev@lists.secondlife.com',
-    url='http://wiki.secondlife.com/wiki/Eventlet',
-    packages=find_packages('eventlet'),
-    long_description="""
-    Eventlet is a networking library written in Python. It achieves
-    high scalability by using non-blocking io while at the same time
-    retaining high programmer usability by using coroutines to make
-    the non-blocking io operations appear blocking at the source code
-    level.""",
+setup(name='python-eventlet-0.8',
+      version=__version__,
+      description='Coroutine-based networking library',
+      author='Linden Lab',
+      maintainer="AG Projects",
+      maintainer_email="support@ag-projects.com",
+      url='http://devel.ag-projects.com/repositories/python-eventlet',
+      packages=find_packages('eventlet'),
+      long_description="""
+        Eventlet is a networking library written in Python. It achieves
+        high scalability by using non-blocking io while at the same time
+        retaining high programmer usability by using coroutines to make
+        the non-blocking io operations appear blocking at the source code
+        level.""",
     classifiers=[
-    "License :: OSI Approved :: MIT License",
-    "Programming Language :: Python",
-    "Operating System :: MacOS :: MacOS X",
-    "Operating System :: POSIX",
-    "Topic :: Internet",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "Intended Audience :: Developers",
-    "Development Status :: 4 - Beta"]
-    )
+      "License :: OSI Approved :: MIT License",
+      "Programming Language :: Python",
+      "Operating System :: MacOS :: MacOS X",
+      "Operating System :: POSIX",
+      "Topic :: Internet",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+      "Intended Audience :: Developers",
+      "Development Status :: 4 - Beta"
+    ])
 
