c6bcfb7410151d4c081808ebd488d2dfe2968c0d
Saul Ibarra <saul@ag-projects.com>
2013-10-03 08:22:10 +0000

    Simplify select module

--- eventlib/green/select.py
+++ eventlib/green/select.py
@@ -1,7 +1,5 @@
 __select = __import__('select')
-__ignore = ('poll', 'epoll', 'kqueue', 'kevent')
-for var in (var for var in dir(__select) if var not in __ignore):
-    exec "%s = __select.%s" % (var, var)
+error = __select.error
 from eventlib.api import select
-del __select, __ignore, var
+del __select
 
