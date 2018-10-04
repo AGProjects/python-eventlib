6d01a1b1168a77b7d34dd3ceb2fc6532e193f301
Dan Pascu <dan@ag-projects.com>
2018-10-04 18:29:18 +0000

    Increased version and updated changelog

--- eventlib/__init__.py
+++ eventlib/__init__.py
@@ -17,5 +17,5 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-version_info = (0, 2, 3)
+version_info = (0, 2, 4)
 __version__ = '%s.%s.%s' % version_info
